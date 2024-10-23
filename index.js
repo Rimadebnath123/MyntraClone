bab28f7b556fbec00b62d7921701c3d0e3f25569
Rimadebnath123 <95009497+Rimadebnath123@users.noreply.github.com>
2024-10-23 21:53:57 +0530

    man and women dynamic

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ onLoad();
 function onLoad() {
     let bagItemsStr = localStorage.getItem('bagItems');
     bagItems = bagItemsStr ? JSON.parse(bagItemsStr) : [];
-    displayItemsOnHomePage();
+    // displayItemsOnPage();
     displayBagIcon();
 }
 
@@ -90,32 +90,32 @@ function displayBagIcon() {
     }
 }
 
-function displayItemsOnHomePage() {
-    let itemsContainerElement = document.querySelector('.items-container');
-    if (!itemsContainerElement) {
-        return;
-    }
-    let innerHtml = '';
-    items.forEach(item => {
-        innerHtml += `
-    <div class="item-container">
+// function displayItemsOnPage() {
+//     let itemsContainerElement = document.querySelector('.items-container');
+//     if (!itemsContainerElement) {
+//         return;
+//     }
+//     let innerHtml = '';
+//     items.forEach(item => {
+//         innerHtml += `
+//     <div class="item-container">
     
-      <img class="item-image" src="${item.image}" alt="item image">
-      <div class="rating">
-          ${item.rating.stars} ⭐ | ${item.rating.count}
-      </div>
-      <div class="company-name">${item.company}</div>
-      <div class="item-name">${item.item_name}</div>
-      <div class="price">
-          <span class="current-price">Rs ${item.current_price}</span>
-          <span class="original-price">Rs ${item.original_price}</span>
-          <span class="discount">(${item.discount}% OFF)</span>
-      </div>
-      <button class="btn-add-bag" onclick="addToBag(${item.id})">Add to Bag</button>
+//       <img class="item-image" src="${item.image}" alt="item image">
+//       <div class="rating">
+//           ${item.rating.stars} ⭐ | ${item.rating.count}
+//       </div>
+//       <div class="company-name">${item.company}</div>
+//       <div class="item-name">${item.item_name}</div>
+//       <div class="price">
+//           <span class="current-price">Rs ${item.current_price}</span>
+//           <span class="original-price">Rs ${item.original_price}</span>
+//           <span class="discount">(${item.discount}% OFF)</span>
+//       </div>
+//       <button class="btn-add-bag" onclick="addToBag(${item.id})">Add to Bag</button>
         
-    </div>`
-    });
-    itemsContainerElement.innerHTML = innerHtml;
-}
+//     </div>`
+//     });
+//     itemsContainerElement.innerHTML = innerHtml;
+// }
 
 
