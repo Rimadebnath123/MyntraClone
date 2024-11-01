581ea9dfdfc78f99060ed4b4ae37c25bae52d0ca
Rimadebnath123 <95009497+Rimadebnath123@users.noreply.github.com>
2024-11-02 00:32:59 +0530

    header and footer full complete

--- index.js
+++ index.js
@@ -80,42 +80,28 @@ function addToBag(itemId) {
     displayBagIcon();
 }
 
-function displayBagIcon() {
-    let bagItemCountElement = document.querySelector('.bag-item-count');
-    if (bagItems.length > 0) {
-        bagItemCountElement.style.visibility = 'visible';
-        bagItemCountElement.innerText = bagItems.length;
-    } else {
-        bagItemCountElement.style.visibility = 'hidden';
-    }
-}
-
-// function displayItemsOnPage() {
-//     let itemsContainerElement = document.querySelector('.items-container');
-//     if (!itemsContainerElement) {
-//         return;
+// function displayBagIcon() {
+//     let bagItemCountElement = document.querySelector('.bag-item-count');
+//     if (bagItems.length > 0) {
+//         bagItemCountElement.style.visibility = 'visible';
+//         bagItemCountElement.innerText = bagItems.length;
+//     } else {
+//         bagItemCountElement.style.visibility = 'hidden';
 //     }
-//     let innerHtml = '';
-//     items.forEach(item => {
-//         innerHtml += `
-//     <div class="item-container">
-    
-//       <img class="item-image" src="${item.image}" alt="item image">
-//       <div class="rating">
-//           ${item.rating.stars} ⭐ | ${item.rating.count}
-//       </div>
-//       <div class="company-name">${item.company}</div>
-//       <div class="item-name">${item.item_name}</div>
-//       <div class="price">
-//           <span class="current-price">Rs ${item.current_price}</span>
-//           <span class="original-price">Rs ${item.original_price}</span>
-//           <span class="discount">(${item.discount}% OFF)</span>
-//       </div>
-//       <button class="btn-add-bag" onclick="addToBag(${item.id})">Add to Bag</button>
-        
-//     </div>`
-//     });
-//     itemsContainerElement.innerHTML = innerHtml;
 // }
 
+function displayBagIcon() {
+    let bagItemCountElement = document.querySelector('.bag-item-count');
+
+    if (bagItemCountElement) {  // Check if element exists
+        if (bagItems.length > 0) {
+            bagItemCountElement.style.visibility = 'visible';
+            bagItemCountElement.innerText = bagItems.length;
+        } else {
+            bagItemCountElement.style.visibility = 'hidden';
+        }
+    } else {
+        console.error('Bag item count element not found');
+    }
+}
 
